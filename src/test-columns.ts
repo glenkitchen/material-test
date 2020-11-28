a30216d8dd0e4156c3be1678950203aee88dc55a
glenkitchen <gkitchen@insightconsulting.co.za>
2020-11-28 08:35:06 +0200

    Initial commit

---
+++ src/test-columns.ts
@@ -0,0 +1,204 @@
+import { Column } from "./material-table/types";
+
+export const testColumns: Column<object>[] = [
+  // Dates
+  {
+    field: "year",
+    title: "Year",
+    width: 120,
+  },
+  {
+    field: "month",
+    title: "Month",
+    width: 120,
+  },
+  {
+    field: "startDate",
+    title: "Start Date",
+    width: 120,
+  },
+  {
+    field: "endDate",
+    title: "End Date",
+    width: 120,
+  },
+  {
+    field: "tbc",
+    title: "TBC",
+    lookup: { true: "Yes", false: "No" },
+    width: 120,
+  },
+  {
+    field: "createdDate",
+    title: "Created Date",
+    width: 120,
+  },
+  {
+    field: "fiscalYear",
+    title: "Fiscal Year",
+    width: 120,
+  },
+
+  // Promotion
+  //   {
+  //     field: "promotionId",
+  //     title: "Event Id",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "eventName",
+  //     title: "Event Name",
+  //     width: 300,
+  //   },
+  //   {
+  //     field: "promotionStatusName",
+  //     title: "Status",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "createdBy",
+  //     title: "Created By",
+  //     width: 300,
+  //   },
+
+  //   // Customer
+  //   {
+  //     field: "customerType",
+  //     title: "Customer Type",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "majorGroup",
+  //     title: "Major Group",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "majorChain",
+  //     title: "Major Chain",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "subChain",
+  //     title: "Sub Chain",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "custRegion",
+  //     title: "Customer Region",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "custName",
+  //     title: "Customer Name",
+  //     width: 300,
+  //   },
+
+  //   // Product
+  //   {
+  //     field: "businessUnit",
+  //     title: "Business Unit",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "category",
+  //     title: "Category",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "subCategory",
+  //     title: "Sub Category",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "brand",
+  //     title: "Brand",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "packSizeName",
+  //     title: "Pack Size",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "productName",
+  //     title: "Product Name",
+  //     width: 300,
+  //   },
+  //   {
+  //     field: "titan",
+  //     title: "Titan",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "multiBuy",
+  //     title: "MultiBuy",
+  //     width: 120,
+  //     type: "boolean",
+  //   },
+  //   {
+  //     field: "comments",
+  //     title: "Comments",
+  //     width: 120,
+  //   },
+
+  //   // Amount Types
+  //   {
+  //     field: "marginSupport",
+  //     title: "Margin Support",
+  //     width: 120,
+  //     type: "boolean",
+  //   },
+  //   {
+  //     field: "marginSupportAmount",
+  //     title: "Margin Amount",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "marginSupportPromotionSpendType",
+  //     title: "Margin Spend Type",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "sally",
+  //     title: "Sally",
+  //     width: 120,
+  //     type: "boolean",
+  //   },
+  //   {
+  //     field: "sallyAmount",
+  //     title: "Sally Amount",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "sallyPromotionSpendType",
+  //     title: "Sally Spend Type",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "tally",
+  //     title: "Tally",
+  //     width: 120,
+  //     type: "boolean",
+  //   },
+  //   {
+  //     field: "tallyAmount",
+  //     title: "Tally Amount",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "tallyPromotionSpendType",
+  //     title: "Tally Spend Type",
+  //     width: 120,
+  //   },
+  //   {
+  //     field: "rsp",
+  //     title: "N/a",
+  //     width: 120,
+  //     type: "boolean",
+  //   },
+  //   {
+  //     field: "rspPromotionSpendType",
+  //     title: "N/a Spend Type",
+  //     width: 120,
+  //   },
+];
